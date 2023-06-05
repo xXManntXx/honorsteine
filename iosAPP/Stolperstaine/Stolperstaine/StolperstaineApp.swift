75abe62f0c1add4f8355b1ec8a9005a40591b6ad
Viktor Velizarov <102257718+Viktor1722@users.noreply.github.com>
2023-06-05 15:09:52 +0200

    Uploading the project directory

---
+++ iosAPP/Stolperstaine/Stolperstaine/StolperstaineApp.swift
@@ -0,0 +1,17 @@
+//
+//  StolperstaineApp.swift
+//  Stolperstaine
+//
+//  Created by Viktor on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct StolperstaineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
